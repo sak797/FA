a3b22a2f44b12134fc4512d82842aed62a43ee7d
Anirudh <kanirudh54@gmail.com>
2018-09-23 23:17:57 -0400

    Added Count sort

--- sort.py
+++ sort.py
@@ -42,12 +42,28 @@ def merge_sort(lis):
         return merge(merge_sort(lis[:mid]), merge_sort(lis[mid:]))
     return []
 
-
+def count_sort(input_array, range_input):
+    output = [0]*len(input_array)
+    count = [0]*range_input
+    # Count
+    for number in input_array:
+        count[number] +=1
+    # cumulative sum
+    for ind in range(1,range_input):
+        count[ind] += count[ind-1]
+    # Keep the output at correct place
+    for ind in range(len(input_array)-1, -1, -1):
+        output[count[input_array[ind]]-1] = input_array[ind]
+        count[input_array[ind]] -= 1
+    return output
 
 if __name__ == '__main__':
-    lis = [4,5,2,3,9,1]
+    lis = [4, 5, 2, 3, 9, 1]
     sort_lis = insertion_sort(lis)
     print(sort_lis)
     lis = [4, 5, 2, 3, 9, 1]
     sort_lis = merge_sort(lis)
+    print(sort_lis)
+    lis = [4, 5, 2, 3, 9, 1]
+    sort_lis = count_sort(lis,11)
     print(sort_lis)
