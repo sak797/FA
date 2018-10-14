7b880e721fd8b5d3bb0e0eb0b66c07dc7a088fef
Anirudh <kanirudh54@gmail.com>
2018-10-13 20:42:00 -0400

    BFS, DFS, Topological sort, Kruskal and Prims MST

--- graph.py
+++ graph.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import heapq
+import unionfind
 
 class Graph:
     def __init__(self):
@@ -56,6 +58,72 @@ class Graph:
                 self.topologicalSortUtil(i, visited, stack)
         return stack
 
+class UGraph():
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v, w):
+        self.graph[u].append([v, w])
+        self.graph[v].append([u, w])
+
+    def PrimMST(self): # doesn't have the complexity ElogV. It is more. I need to use specialized heap where I know the positions of my vertices in heap in O(1)
+        heap = []
+        for vertex in list(self.graph):
+            heapq.heappush(heap, [9999, vertex])
+        first_vertex = heap[0]
+        first_vertex[0] = 0
+        heap[0] = first_vertex
+        heapq.heapify(heap)
+        parent= defaultdict(int)
+        parent[first_vertex[1]] = -1
+        while len(heap):
+            node = heapq.heappop(heap)
+            # find the dnode in heap and update the distance
+            for dnode in self.graph[node[1]]:
+                dest, dist = dnode
+                for i in range(len(heap)):
+                    if heap[i][1] == dest:
+                        if heap[i][0] > dist:
+                            heap[i][0] = dist
+                            parent[dest] = node[1]
+                            heapq.heapify(heap)
+                            break
+        for i in list(parent):
+            print(i, " --> ", parent[i], end="\n")
+
+    def KruskalMST(self):
+        result = []
+        count1 = 0
+        count2 = 0
+        # convert self.graph to [u,v,w]
+        unique_edges = set()
+        for svertex in list(self.graph):
+            for dvertex in self.graph[svertex]:
+                temp1 = (svertex, dvertex[0], dvertex[1])
+                temp2 = (dvertex[0], svertex, dvertex[1])
+                if temp1 in unique_edges or temp2 in unique_edges:
+                    pass
+                else:
+                    unique_edges.add(temp1)
+
+        unique_edges = sorted(unique_edges, key = lambda x: x[2])
+        print(unique_edges)
+        uf = unionfind.UF(len(self.graph))
+        while count1 < len(self.graph) and count2 < len(unique_edges):
+            temp = unique_edges[count2]
+            x = uf.find(temp[0])
+            y = uf.find(temp[1])
+            count2 += 1
+            if x != y:
+                uf.union(x,y)
+                result.append(temp)
+                count1 += 1
+        for item in result:
+            print(item[0], " --> ", item[1])
+
+
+
+
 
 if __name__ == '__main__':
     g = Graph()
@@ -79,3 +147,22 @@ if __name__ == '__main__':
     G.addEdge(3, 1)
     out = G.topologicalSort()
     print(out, end="\n")
+
+    u = UGraph()
+    u.addEdge(0, 1, 4)
+    u.addEdge(0, 7, 8)
+    u.addEdge(1, 2, 8)
+    u.addEdge(1, 7, 11)
+    u.addEdge(2, 3, 7)
+    u.addEdge(2, 8, 2)
+    u.addEdge(2, 5, 4)
+    u.addEdge(3, 4, 9)
+    u.addEdge(3, 5, 14)
+    u.addEdge(4, 5, 10)
+    u.addEdge(5, 6, 2)
+    u.addEdge(6, 7, 1)
+    u.addEdge(6, 8, 6)
+    u.addEdge(7, 8, 7)
+    u.PrimMST()
+    print("Kruskal: ")
+    u.KruskalMST()
